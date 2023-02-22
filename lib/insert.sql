59d9e321c428c0b957c23203c86bcddc58ddf0cb
Preston <prestonosoro56@gmail.com>
2023-02-22 17:12:29 +0300

    feat: create and insert rows

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate",4,"M","white brown", "LOYALITY",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy",5,"F","brown","PLAYFUL",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha",7,"F","white","LOVING",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergent Brown",6,"M","black","DANGEROUS",1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa",8,"F","dark brown","VIGILANT",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch",9,"M","dark grey","FURIOUS",0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy",5,"F","black","CARING",1);
+INSERT INTO bears (name) VALUES (NULL);
